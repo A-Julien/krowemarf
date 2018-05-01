c45dd9342ec15f8c94733fa8f80d4ea7baa27361
Alaimo <julien.alaimo@gmail.com>
2018-05-01 14:12:38 +0200

    adding service and struct module

---
+++ krowemarf-kernel/krowemarf-services/src/main/java/com/prckt/krowemarf/services/App.java
@@ -0,0 +1,13 @@
+package com.prckt.krowemarf.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
