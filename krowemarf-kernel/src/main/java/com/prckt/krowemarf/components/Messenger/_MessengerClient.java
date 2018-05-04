1d1f44ec2ea9c770521e1994ad4a771486455c8c
Alaimo <julien.alaimo@gmail.com>
2018-05-04 09:27:13 +0200

    add DocumentLibrary Module

--- krowemarf-kernel/src/main/java/com/prckt/krowemarf/components/Messenger/_MessengerClient.java
+++ krowemarf-kernel/src/main/java/com/prckt/krowemarf/components/Messenger/_MessengerClient.java
@@ -6,5 +6,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface _MessengerClient<T extends _DefaultMessage> extends Remote {
-    public void displayMessage(T message) throws RemoteException;
+    public void onReceive(T message) throws RemoteException;
 }
