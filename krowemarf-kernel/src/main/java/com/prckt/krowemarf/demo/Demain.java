76c850aa981108b2e8ca583ca2cda500bf13b3b5
Sébastien LEMPEREUR <lmpsebastien1337@gmail.com>
2018-05-08 14:59:09 +0200

    Début chat

---
+++ krowemarf-kernel/src/main/java/com/prckt/krowemarf/demo/Demain.java
@@ -0,0 +1,29 @@
+package com.prckt.krowemarf.demo;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class Demain {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(){
+        try {
+            Stage primaryStage = new Stage();
+            VBox root = (VBox)FXMLLoader.load(getClass().getResource("demo.fxml"));
+            Scene scene = new Scene(root,800,500);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ krowemarf-kernel/src/main/java/com/prckt/krowemarf/demo/DemoController.java
@@ -0,0 +1,18 @@
+package com.prckt.krowemarf.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+import java.awt.*;
+
+public class DemoController {
+    @FXML
+    private TextArea messageArea;
+
+    @FXML
+    private ListView<String> liste;
+
+    @FXML
+    private TextField inputField;
+
+}
